101d4e65df4e9327d33381cda69c075b97325183
fhsh0300243 <lilisbg.lee@gmail.com>
2020-11-11 22:24:36 +0800

    featurw(STS): first commit

---
+++ src/main/java/model/UsersModel.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@Entity
+public class UsersModel {
+  
+  @JsonProperty("usersId")
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(columnDefinition = "serial", name = "id" )
+  private Long id;
+  
+  @JsonProperty("account")
+  @Column(name = "account")
+  private String account;
+  
+  @JsonProperty("passwordHash")
+  @Column(name = "password_hash")
+  private String passwordHash;
+  
+  @JsonProperty("accessToken")
+  @Column(name = "access_token")
+  private String accessToken;
+  
+  @JsonProperty("balance")
+  @Column(name = "balance")
+  private BigDecimal balance;
+  
+  @JsonProperty("email")
+  @Column(name = "email")
+  private String email;
+  
+  @JsonProperty("status")
+  @Column(name = "status")
+  private int status;
+}
